bf9a09957ec9a35ecb9f89e4fc759996739c2a89
ShrinivasSH <sholgundikar@gmail.com>
2023-05-21 22:07:42 +0530

    Added npm and parcel to project

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
     // const heading= React.createElement("h1",{id":"title"},"Namaste Everyone from React!");
     const root= ReactDOM.createRoot(document.getElementById("root"));
-    const h1= React.createElement("h1",{},"Heading One!");
+    const h1= React.createElement("h1",{},"Heading One from parcel!");
     const h2= React.createElement("h2",{},"Heading two!");
     const container= React.createElement("div",{id:"container"},[h1,h2])
     root.render(container);
